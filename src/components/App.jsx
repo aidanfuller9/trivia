c18988aaafd8f838e67490483682eed0549e558d
aidanfuller9 <60492278+aidanfuller9@users.noreply.github.com>
2022-02-09 17:53:10 -0800

    work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,19 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  return (
+    <div className="app">
+      Trivia!
+      <Question value={data[0].question.text} />
+    </div>
+  );
+}
+
+class Question extends React.Component {
+  render() {
+    var question = this.props.value;
+    return <h1>{question}</h1>;
+  }
 }
 
 export default App;
