68f95b193215c063690cf43b456d5b9265118660
amandayu255 <69171765+amandayu255@users.noreply.github.com>
2022-03-02 17:59:37 -0800

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,18 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  var questionNum = 0;
+
+  return (
+    <div className="app">
+      Trivia!
+      <Question text={data[0].question.text} />
+    </div>
+  );
 }
 
 export default App;
+
+function Question(props) {
+  return <h1>{props.text}</h1>;
+}
